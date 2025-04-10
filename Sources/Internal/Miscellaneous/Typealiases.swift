3cb2b56003910f765d7a5d39d2c2cb49717519cf
FutureSeth <158508122+FutureSeth@users.noreply.github.com>
2025-04-09 23:35:25 -0600

    Patch 3.0.2
    fix:
    
    Swift 6.1 broke package compilation with complaints about builders switching actors. Updating the builder typealias to specify MainActor.

--- Sources/Internal/Miscellaneous/Typealiases.swift
+++ Sources/Internal/Miscellaneous/Typealiases.swift
@@ -11,6 +11,6 @@
 
 import SwiftUI
 
-public typealias CameraScreenBuilder = (CameraManager, Namespace.ID, _ closeMCameraAction: @escaping () -> ()) -> any MCameraScreen
-public typealias CapturedMediaScreenBuilder = (MCameraMedia, Namespace.ID, _ retakeAction: @escaping () -> (), _ acceptMediaAction: @escaping () -> ()) -> any MCapturedMediaScreen
-public typealias ErrorScreenBuilder = (MCameraError, _ closeMCameraAction: @escaping () -> ()) -> any MCameraErrorScreen
+public typealias CameraScreenBuilder = @MainActor (CameraManager, Namespace.ID, _ closeMCameraAction: @escaping () -> ()) -> any MCameraScreen
+public typealias CapturedMediaScreenBuilder = @MainActor (MCameraMedia, Namespace.ID, _ retakeAction: @escaping () -> (), _ acceptMediaAction: @escaping () -> ()) -> any MCapturedMediaScreen
+public typealias ErrorScreenBuilder = @MainActor (MCameraError, _ closeMCameraAction: @escaping () -> ()) -> any MCameraErrorScreen
